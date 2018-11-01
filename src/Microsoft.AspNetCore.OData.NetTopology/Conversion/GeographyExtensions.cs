a5da82b2be7933acfe61eb04c8f267b09aa9b8f8
Josh Comley <joshcomley@googlemail.com>
2018-11-01 11:49:43 +0000

    Ordering of points clockwise or counter-clockwise no longer functionally changes polygon sequence

--- src/Microsoft.AspNetCore.OData.NetTopology/Conversion/GeographyExtensions.cs
+++ src/Microsoft.AspNetCore.OData.NetTopology/Conversion/GeographyExtensions.cs
@@ -127,12 +127,18 @@ namespace Microsoft.AspNetCore.OData.NetTopology.Conversion
 
             var geomFactory = new GeometryFactory(new PrecisionModel(), 4326);
             coords.RemoveAt(coords.Count - 1);
-            coords.Sort(new CoordinateComparer(CalculateCentre(coords)));
+            //coords.Sort(new CoordinateComparer(CalculateCentre(coords)));
             var first = coords.First();
             coords.Add(new Coordinate(first.X, first.Y, first.Z));
+            var sequence = new CoordinateArraySequence(coords.ToArray());
             var poly = new Polygon(
-                new LinearRing(new CoordinateArraySequence(coords.ToArray()), geomFactory),
+                new LinearRing(sequence, geomFactory),
                 geomFactory);
+            // Reverse if this is counter clock-wise
+            if (poly.Shell.IsCCW)
+            {
+                poly.Shell.Reverse();
+            }
             return poly;
         }
 
@@ -146,21 +152,31 @@ namespace Microsoft.AspNetCore.OData.NetTopology.Conversion
             var builder = SpatialImplementation.CurrentImplementation.CreateBuilder();
             builder.GeographyPipeline.SetCoordinateSystem(CoordinateSystem.DefaultGeography);
             builder.GeographyPipeline.BeginGeography(SpatialType.Polygon);
-            BuildRing(polygon.ExteriorRing, builder);
+            var exteriorRing = polygon.ExteriorRing;
+            if (!new LinearRing(exteriorRing.Coordinates).IsCCW)
+            {
+                exteriorRing = (ILineString) exteriorRing.Reverse();
+            }
+            BuildRing(exteriorRing, builder);
             foreach (var ring in polygon.Holes)
             {
-                BuildRing(ring, builder);
+                var ringCopy = ring;
+                if (ringCopy.IsCCW)
+                {
+                    ringCopy = (ILinearRing) ringCopy.Reverse();
+                }
+                BuildRing(ringCopy, builder);
             }
 
             builder.GeographyPipeline.EndGeography();
             return (GeographyPolygon) builder.ConstructedGeography;
         }
 
-        private static void BuildRing(IGeometry ring, SpatialPipeline builder)
+        private static void BuildRing(ILineString ring, SpatialPipeline builder)
         {
             var coords = ring.Coordinates.ToList();
             coords.RemoveAt(coords.Count - 1);
-            coords.Sort(new CoordinateComparer(CalculateCentre(coords)));
+            //coords.Sort(new CoordinateComparer(CalculateCentre(coords)));
             var first = coords.First();
             builder.GeographyPipeline.BeginFigure(new GeographyPosition(first.Y, first.X, first.Z, null));
             for (var i = 1; i < coords.Count; i++)
@@ -187,60 +203,60 @@ namespace Microsoft.AspNetCore.OData.NetTopology.Conversion
             return new Coordinate(centerX, centerY);
         }
 
-        private class CoordinateComparer : IComparer<Coordinate>
-        {
-            private const double FloatingPointTolerance = 0.00001;
+        //private class CoordinateComparer : IComparer<Coordinate>
+        //{
+        //    private const double FloatingPointTolerance = 0.00001;
 
-            public CoordinateComparer(Coordinate center)
-            {
-                Center = center;
-            }
+        //    public CoordinateComparer(Coordinate center)
+        //    {
+        //        Center = center;
+        //    }
 
-            private Coordinate Center { get; }
+        //    private Coordinate Center { get; }
 
-            public int Compare(Coordinate a, Coordinate b)
-            {
-                Debug.Assert(a != null);
-                Debug.Assert(b != null);
-                if (a.X - Center.X >= 0 && b.X - Center.X < 0)
-                {
-                    return 1;
-                }
+        //    public int Compare(Coordinate a, Coordinate b)
+        //    {
+        //        Debug.Assert(a != null);
+        //        Debug.Assert(b != null);
+        //        if (a.X - Center.X >= 0 && b.X - Center.X < 0)
+        //        {
+        //            return 1;
+        //        }
 
-                if (a.X - Center.X < 0 && b.X - Center.X >= 0)
-                {
-                    return 1;
-                }
+        //        if (a.X - Center.X < 0 && b.X - Center.X >= 0)
+        //        {
+        //            return 1;
+        //        }
 
-                if (Math.Abs(a.X - Center.X) < FloatingPointTolerance &&
-                    Math.Abs(b.X - Center.X) < FloatingPointTolerance)
-                {
-                    if (a.Y - Center.Y >= 0 || b.Y - Center.Y >= 0)
-                    {
-                        return a.Y > b.Y ? 1 : -1;
-                    }
+        //        if (Math.Abs(a.X - Center.X) < FloatingPointTolerance &&
+        //            Math.Abs(b.X - Center.X) < FloatingPointTolerance)
+        //        {
+        //            if (a.Y - Center.Y >= 0 || b.Y - Center.Y >= 0)
+        //            {
+        //                return a.Y > b.Y ? 1 : -1;
+        //            }
 
-                    return b.Y > a.Y ? 1 : -1;
-                }
+        //            return b.Y > a.Y ? 1 : -1;
+        //        }
 
-                // compute the cross product of vectors (center -> a) x (center -> b)
-                var det = (a.X - Center.X) * (b.Y - Center.Y) - (b.X - Center.X) * (a.Y - Center.Y);
-                if (det < 0)
-                {
-                    return 1;
-                }
+        //        // compute the cross product of vectors (center -> a) x (center -> b)
+        //        var det = (a.X - Center.X) * (b.Y - Center.Y) - (b.X - Center.X) * (a.Y - Center.Y);
+        //        if (det < 0)
+        //        {
+        //            return 1;
+        //        }
 
-                if (det > 0)
-                {
-                    return -1;
-                }
+        //        if (det > 0)
+        //        {
+        //            return -1;
+        //        }
 
-                // Points a and b are on the same line from the center
-                // check which Point is closer to the center
-                var d1 = (a.X - Center.X) * (a.X - Center.X) + (a.Y - Center.Y) * (a.Y - Center.Y);
-                var d2 = (b.X - Center.X) * (b.X - Center.X) + (b.Y - Center.Y) * (b.Y - Center.Y);
-                return d1 > d2 ? 1 : -1;
-            }
-        }
+        //        // Points a and b are on the same line from the center
+        //        // check which Point is closer to the center
+        //        var d1 = (a.X - Center.X) * (a.X - Center.X) + (a.Y - Center.Y) * (a.Y - Center.Y);
+        //        var d2 = (b.X - Center.X) * (b.X - Center.X) + (b.Y - Center.Y) * (b.Y - Center.Y);
+        //        return d1 > d2 ? 1 : -1;
+        //    }
+        //}
     }
 }
